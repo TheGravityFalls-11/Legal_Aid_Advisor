3d12d5ee429a360de2ad302cb9922f6ed0d157c6
TheGravityFalls-11 <hotwaniaastha25@gmail.com>
2025-04-08 21:52:50 +0530

    Update urls.py

--- chatbot_project/frontend/urls.py
+++ chatbot_project/frontend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, chat_page, signlogin, booking, case_study,lawyer
+from .views import index, chat_page, signlogin, booking, case_study,lawyer,court,knowledge
 
 urlpatterns = [
     path("", signlogin, name="signlogin"),  # Signup/Login Page
@@ -8,4 +8,7 @@ urlpatterns = [
     path("case/", case_study, name="case"),  # Case Study Page (assuming case_study is the correct view)
     path("booking/", booking, name="booking"),  # Booking Page
     path("lawyer/", lawyer, name="lawyer"), 
+    path("knowledge/", knowledge, name="knowledge"), 
+    path("court/", court, name="court") 
+
 ]
